b9134c5c51ee48fcc6ebf3a9630b570c884d0de9
Sean Ryan <seanpryan89@gmail.com>
2019-05-29 19:56:05 -0400

    users/signup route still not displaying properly.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,5 @@
 module.exports = {
     signUp(req, res, next) {
-        console.log("In User Controller Sign Up")
         res.send("test");
     }
 }
--- src/routes/static.js
+++ src/routes/static.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log("Got a get request")
+  res.send("Test");
+});
+
+router.get("/users/signup", (req, res, next) => {
+  console.log("Got a get request")
   res.send("Welcome to Blocipedia");
+  res.status(404).send("Sorry, can't find that.")
 });
 
 module.exports = router;
