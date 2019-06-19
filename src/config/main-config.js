37ddffc3dfcf75f0afe926015b279463e77b47dc
Sean Ryan <seanpryan89@gmail.com>
2019-06-18 22:22:16 -0400

    changed database location in config file

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -8,6 +8,8 @@ const session = require("express-session");
 const flash = require("express-flash");
 const passportConfig = require("./passport-config");
 const logger = require('morgan');
+const pg = require('pg');
+const connectionString = "postgres://vimbcjrehweypj:0e015bf853ae8c9ded68d7c10cf3b087552ea3a02f3a575ecabec235af953857@ec2-174-129-208-118.compute-1.amazonaws.com:5432/d9sva8g7qltint"
 
 module.exports = {
   init(app, express){
